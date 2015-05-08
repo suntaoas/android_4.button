84b2628e712cfd97dd4b82997984712d7a3676b9
Tao Sun <suntaoas2014@gmail.com>
2015-05-07 23:54:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/suntao/myapplication1/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.suntao.myapplication1;
+
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Button btn1;
+    private Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn1.setOnClickListener(listener);
+        btn2.setOnClickListener(listener);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button btn = (Button) v;
+            switch (btn.getId())
+            {
+                case R.id.btn1:
+                    Toast.makeText(MainActivity.this,"You clicked button btn1 !",Toast.LENGTH_LONG).show();
+                    System.out.println("You clicked button btn1 !");
+                    break;
+                case R.id.btn2:
+                    Toast.makeText(MainActivity.this,"You clicked button btn2 !",Toast.LENGTH_LONG).show();
+                    System.out.println("You clicked button btn2 !");
+            }
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
